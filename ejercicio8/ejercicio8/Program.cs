c083e7a1396a47296fc96b91daa451df67144475
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-21 11:07:36 -0500

    Ejercicio finalizado

---
+++ ejercicio8/ejercicio8/Program.cs
@@ -0,0 +1,23 @@
+﻿public class ejercicio8
+{
+    static void Main(string[] args)
+    {
+        //Construir un programa que pida un número y luego diga si este número es par o no.
+
+        int num;
+
+        Console.Write("Ingrese su numero:");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0) 
+        {
+            Console.Write($"el numero {num} es par");
+        }
+        else 
+        { Console.Write($"El numero {num} es impar"); }
+        
+
+
+    }
+
+
+}
