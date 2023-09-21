7700dbef0e5e45816412cc9c226d6899cb2bc705
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-21 10:37:25 -0500

    Ejercicio finalizado

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,47 @@
+﻿public class ejercicio7 
+{
+
+    static void Main(string[] args)
+    {
+        //Escribir un algoritmo que dados 2 valores de entrada imprima siempre la división del mayor entre el menor.
+
+        int num1, num2;
+        double cociente;
+
+        Console.Write("Ingrese su primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Ingrese su segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+
+        if (num1 == 0)
+        {
+            cociente = num1 / num2;
+
+        }
+        else if (num2 == 0)
+        {
+            cociente=num2 / num1;
+        }
+ 
+        if (num1 > num2)
+        {
+            cociente = num1 / num2;
+        }
+        else { cociente = num2 / num1;}
+
+        if (num2 == 0 & num1 == 0)
+        {
+            Console.Write("Indeterminado");
+        }
+
+        Console.Write($"El cociente es {0}", cociente);
+
+
+    }
+
+
+
+
+}
+
